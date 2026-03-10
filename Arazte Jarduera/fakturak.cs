7b7423c351b28a1964ca883ab8be067dca8a9ba4
metxabe25-ship-it <metxabe25@izarraitz.eus>
2026-03-10 11:59:14 +0100

    Fakturen inporte berriak txertatu: 400, 500, 800

--- Arazte Jarduera/fakturak.cs
+++ Arazte Jarduera/fakturak.cs
@@ -6,7 +6,7 @@ class Program
 {
     static void Main()
     {
-        List<int> fakturak = new List<int> { 120, 50, 300, 80, 200 };
+        List<int> fakturak = new List<int> { 120, 50, 300, 80, 200, 400, 500, 800 };
         int deskontua = 10;
         int guztira = 0;
 
